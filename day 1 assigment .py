e8530e4af459ada96b3315bdf43e29e605121a5f
Mohammed Junaid Ahmed <77689871+junaidtheengineer@users.noreply.github.com>
2021-12-21 00:47:31 +0530

    Assigment  day 1

---
+++ day 1 assigment .py
@@ -0,0 +1,5 @@
+Principal = float(input("Enter the principal amount:  "))
+rate = float(input("Enter the rate:  "))
+time = float(input("Enter the time period:  "))
+simple_interst = (Principal*rate*time)/100
+print("the amount will be:", simple_interst ,"by SI formula")
